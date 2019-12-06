9e48b98d415fd52ea9e681a253d4328adf3bcc4a
BennEntterprise <kyle@bennentterprise.com>
2019-12-06 13:13:53 -0500

    eov58 use jwtHelper in Auth to persist UI variable

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -9,6 +9,7 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
   jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
   constructor(private http: HttpClient) { }
 
@@ -19,13 +20,15 @@ export class AuthService {
           const user = response;
           if (user) {
             localStorage.setItem('token', user.token);
+            this.decodedToken = this.jwtHelper.decodeToken(user.token);
+            console.log(this.decodedToken);
           }
         })
       );
   }
 
   register(model: any) {
-    return this.http.post(this.baseUrl + 'register' , model);
+    return this.http.post(this.baseUrl + 'register', model);
   }
 
   loggedIn() {
