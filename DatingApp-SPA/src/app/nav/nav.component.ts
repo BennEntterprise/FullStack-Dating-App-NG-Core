0dfd23e3667e38e11e4ab81d8e97e10606cc5785
BennEntterprise <kyle@bennentterprise.com>
2019-12-06 11:53:22 -0500

    eov57 add alertifyService for UI feedback toasts

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,27 +9,27 @@ import { AuthService } from '../_services/auth.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login() {
     this.authService.login(this.model).subscribe(next => {
-      console.log('Logged In Successfully');
+      this.alertify.success('Logged In Successfully');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token'); 
+    const token = localStorage.getItem('token');
     return !!token; // If anything is in the token return true, otherwise false
   }
 
   logout() {
-    localStorage.removeItem('token'); 
-    console.log('Logged out')
+    localStorage.removeItem('token');
+    this.alertify.message('Logged out');
   }
 
 }
