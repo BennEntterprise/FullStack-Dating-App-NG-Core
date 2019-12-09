61dc3c0e358ba2be5719db07dab75da2e0afe0ea
BennEntterprise <kyle@bennentterprise.com>
2019-12-08 20:07:29 -0500

    eov84 (attempt 2) new user service

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -6,10 +6,9 @@ import { User } from '../_models/user';
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Authorization': 'Bearer ' + localStorage.getItem('token')
+    Authorization: 'Bearer ' + localStorage.getItem('token')
   })
 };
-
 
 @Injectable({
   providedIn: 'root'
@@ -17,11 +16,13 @@ const httpOptions = {
 export class UserService {
   baseUrl = environment.apiUrl;
 
+
   constructor(private http: HttpClient) { }
 
   getUsers(): Observable<User[]> {
     return this.http.get<User[]>(this.baseUrl + 'users', httpOptions);
   }
+
 
   getUser(id): Observable<User> {
     return this.http.get<User>(this.baseUrl + 'users/' + id, httpOptions);
