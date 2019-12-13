610b6bd21f1eeea1a0927464be6c0daf0abe1fc0
BennEntterprise <kyle@bennentterprise.com>
2019-12-13 13:30:00 -0500

    eov99 continue working on ui/ux for edit-member
    - add feedback for when form has unsaved changes.
    - hook button to form to handle submit

--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/_models/user';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-member-edit',
@@ -8,9 +10,10 @@ import { User } from 'src/app/_models/user';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm', {static: true}) editForm: NgForm; // @Decorator('nameOnHtml', resolveQueryResultsBeforeChangeDetection) nameWeUseInThisFile: Type
   user: User;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private alertify : AlertifyService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
@@ -18,5 +21,11 @@ export class MemberEditComponent implements OnInit {
     });
   }
 
+  updateUser(){
+    console.log(this.user)
+    this.alertify.success('Profile Updated Successfully');
+    this.editForm.reset(this.user); // Undoes tghe 'dirty state' and sets it to the value as saved
+
+  }
 
 }
