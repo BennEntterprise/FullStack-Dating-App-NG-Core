2bdb1ff9ca041a93ed153eeb0579b8c34552b7fe
BennEntterprise <kyle@bennentterprise.com>
2019-12-15 16:50:15 -0500

    eov108 finish PhotosController logic.
    includes creating a few dtos and maps

--- DatingApp.API/Controllers/PhotosController.cs
+++ DatingApp.API/Controllers/PhotosController.cs
@@ -39,6 +39,16 @@ namespace DatingApp.API.Controllers
             _cloudinary = new Cloudinary(acc);
         }
 
+        [HttpGet("{id}", Name = "GetPhoto")]
+        public async Task<IActionResult>GetPhoto(int id)
+        {
+            var photoFromRepo = await _repo.GetPhoto(id);
+
+            var photo = _mapper.Map<PhotoForReturnDto>(photoFromRepo);
+            
+            return Ok(photo);
+        }
+
         [HttpPost]
         public async Task<IActionResult> AddPhotoForUser(int userId, PhotoForCreationDto photoForCreationDto)
         {
@@ -77,8 +87,10 @@ namespace DatingApp.API.Controllers
 
             userFromRepo.Photos.Add(photo);
 
+
             if(await _repo.SaveAll()){
-                return Ok();
+                var photoToReturn = _mapper.Map<PhotoForReturnDto>(photo);
+                return CreatedAtRoute("GetPhoto", new {userId = userId, id = photo.Id }, photoToReturn );
             }
 
             return BadRequest("Could not add the photo");
