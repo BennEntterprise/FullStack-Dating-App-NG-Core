65ba1e98f50bbb7744bdf2a8c921a46eb2c80826
BennEntterprise <kyle@bennentterprise.com>
2019-12-31 13:54:36 -0500

    eov155 Liker/Likee functionality in API

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -34,7 +34,7 @@ namespace DatingApp.API.Data
 
         public async Task<Photo> GetMainPhotoForUser(int userId)
         {
-            return await _context.Photos.Where( u => u.UserId == userId).FirstOrDefaultAsync(p => p.IsMain);
+            return await _context.Photos.Where(u => u.UserId == userId).FirstOrDefaultAsync(p => p.IsMain);
         }
 
         public async Task<Photo> GetPhoto(int id)
@@ -45,33 +45,45 @@ namespace DatingApp.API.Data
 
         public async Task<User> GetUser(int id)
         {
-            var user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Id ==id);
+            var user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Id == id);
             return user;
         }
 
         public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users =  _context.Users.Include(p => p.Photos).OrderByDescending(u => u.LastActive).AsQueryable(); 
+            var users = _context.Users.Include(p => p.Photos).OrderByDescending(u => u.LastActive).AsQueryable();
 
-            users = users.Where( u => u.Id != userParams.UserId);
+            users = users.Where(u => u.Id != userParams.UserId);
 
             users = users.Where(users => users.Gender == userParams.Gender);
 
-            if(userParams.MinAge != 18 || userParams.MaxAge !=99)
+            if (userParams.Likers)
             {
-                var minDob = DateTime.Today.AddYears(-userParams.MaxAge-1);
-                var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
-                users = users.Where( u => u.DateOfBirth >= minDob && u.DateOfBirth <=maxDob);
+                var userLikers = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = users.Where(u => userLikers.Contains(u.Id));
             }
 
-            if( !string.IsNullOrEmpty(userParams.OrderBy))
+            if (userParams.Likees)
             {
-                switch(userParams.OrderBy)
+                var userLikees = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = users.Where(u => userLikees.Contains(u.Id));
+            }
+
+            if (userParams.MinAge != 18 || userParams.MaxAge != 99)
+            {
+                var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
+                var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
+                users = users.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+            }
+
+            if (!string.IsNullOrEmpty(userParams.OrderBy))
+            {
+                switch (userParams.OrderBy)
                 {
                     case "created":
                         users = users.OrderByDescending(u => u.Created);
-                        break; 
-                    default: 
+                        break;
+                    default:
                         users = users.OrderByDescending(u => u.LastActive);
                         break;
                 }
@@ -79,6 +91,22 @@ namespace DatingApp.API.Data
             return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
         }
 
+        private async Task<IEnumerable<int>> GetUserLikes(int id, bool likers)
+        {
+            var user = await _context.Users
+                .Include(x => x.Liker)
+                .Include(x => x.Likee)
+                .FirstOrDefaultAsync(u => u.Id == id);
+
+            if (likers)
+            {
+                return user.Liker.Where(u => u.LikeeId == id).Select(i => i.LikerId);
+            }
+            else
+            {
+                return user.Likee.Where(u => u.LikerId == id).Select(i => i.LikeeId);
+            };
+        }
         public async Task<bool> SaveAll()
         {
             return await _context.SaveChangesAsync() > 0;
