6c217b1ab41a2cf41a4a2b494f22fa5a95bda2c3
BennEntterprise <kyle@bennentterprise.com>
2019-12-30 20:53:02 -0500

    eov147 Implement UI controlls for filtering

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -16,7 +16,7 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  getUsers(page?, itemsPerPage?): Observable<PaginatedResult<User[]>> {
+  getUsers(page?, itemsPerPage?, userParams?): Observable<PaginatedResult<User[]>> {
     const paginatedResults: PaginatedResult<User[]> = new PaginatedResult<User[]>();
 
     let params = new HttpParams();
@@ -26,12 +26,18 @@ export class UserService {
       params = params.append('pageSize', itemsPerPage);
     }
 
-    return this.http.get<User[]>(this.baseUrl + 'users', { observe: 'response', params})
+    if (userParams != null) {
+      params = params.append('minAge', userParams.minAge);
+      params = params.append('maxAge', userParams.maxAge);
+      params = params.append('gender', userParams.gender);
+    }
+
+    return this.http.get<User[]>(this.baseUrl + 'users', { observe: 'response', params })
       .pipe(
         map(response => {
           paginatedResults.result = response.body;
           if (response.headers.get('Pagination') != null) {
-            paginatedResults.pagination = JSON.parse(response.headers.get('Pagination'))
+            paginatedResults.pagination = JSON.parse(response.headers.get('Pagination'));
           }
           return paginatedResults;
         })
